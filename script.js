d1723727b9fa4fe9a869f17509fafdc10fc2c87e
vidyashreeathani1999 <89911150+vidyashreeathani1999@users.noreply.github.com>
2021-12-02 01:23:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+function printBeverage( name){
+        
+    if(name=="Coca-Cola"){
+        var rate=70;
+        var quantity=document.getElementById("name1").value;
+        var total=quantity*rate;
+        document.getElementById("print1").innerHTML=total;
+       
+    }
+
+    if(name=="Pepsi"){
+        var rate=30;
+        var quantity=document.getElementById("name2").value;
+        var total=quantity*rate;
+        document.getElementById("print2").innerHTML=total;
+       
+    }
+
+    if(name=="Red Bull"){
+       var rate=50;
+        var quantity=document.getElementById("name3").value;
+        var total=quantity*rate;
+        document.getElementById("print3").innerHTML=total;
+       
+    }
+
+    if(name=="Spyci"){
+        var rate=50;
+        var quantity=document.getElementById("name4").value;
+        var total=quantity*rate;
+        document.getElementById("print4").innerHTML=total;
+       
+    }
+
+    if(name=="Sprite"){
+        var rate=50;
+        var quantity=document.getElementById("name5").value;
+        var total=quantity*rate;
+        document.getElementById("print5").innerHTML=total;
+       
+    }
+    
+    if(name=="Mirinda"){
+        var rate=70;
+        var quantity=document.getElementById("name6").value;
+        var total=quantity*rate;
+        document.getElementById("print6").innerHTML=total;
+       
+    }
+    if(name=="Gatorade"){
+        var rate=40;
+        var quantity=document.getElementById("name7").value;
+        var total=quantity*rate;
+        document.getElementById("print7").innerHTML=total;
+       
+    }
+    if(name=="Diet Coke"){
+        var rate=45;
+        var quantity=document.getElementById("name8").value;
+        var total=quantity*rate;
+        document.getElementById("print8").innerHTML=total;
+       
+    }
+    if(name=="Mountain Dew"){
+        var rate=50;
+        var quantity=document.getElementById("name9").value;
+        var total=quantity*rate;
+        document.getElementById("print9").innerHTML=total;
+       
+    }
+    if(name=="Fanta"){
+        var rate=44;
+        var quantity=document.getElementById("name10").value;
+        var total=quantity*rate;
+        document.getElementById("print10").innerHTML=total;
+       
+    }
+    if(name=="Dr Pepper"){
+        var rate=80;
+        var quantity=document.getElementById("name11").value;
+        var total=quantity*rate;
+        document.getElementById("print11").innerHTML=total;
+       
+    }
+
+    if(name=="Tropicana"){
+        var rate=45;
+        var quantity=document.getElementById("name12").value;
+        var total=quantity*rate;
+        document.getElementById("print12").innerHTML=total;
+       
+    }
+    if(name=="7-Up"){
+        var rate=47;
+        var quantity=document.getElementById("name13").value;
+        var total=quantity*rate;
+        document.getElementById("print13").innerHTML=total;
+       
+    }
+
+    if(name=="Maaza"){
+        var rate=30;
+        var quantity=document.getElementById("name14").value;
+        var total=quantity*rate;
+        document.getElementById("print14").innerHTML=total;
+       
+    }
+    if(name=="Coke Zero"){
+        var rate=40;
+        var quantity=document.getElementById("name15").value;
+        var total=quantity*rate;
+        document.getElementById("print15").innerHTML=total;
+       
+    }
+    if(name=="Lemon Juice"){
+        var rate=20;
+        var quantity=document.getElementById("name16").value;
+        var total=quantity*rate;
+        document.getElementById("print16").innerHTML=total;
+       
+    }
+    if(name=="Frooti"){
+        var rate=40;
+        var quantity=document.getElementById("name17").value;
+        var total=quantity*rate;
+        document.getElementById("print17").innerHTML=total;
+       
+    }
+    if(name=="Monster"){
+        var rate=40;
+        var quantity=document.getElementById("name18").value;
+        var total=quantity*rate;
+        document.getElementById("print18").innerHTML=total;
+       
+    }
+    
+}
---
+++ script.ts
@@ -0,0 +1,85 @@
+if(name=="Pepsi"){
+    alert("Amount is Rs/-30");
+    console.log("Amount is Rs/-30");
+}
+
+if(name=="Red Bull"){
+    alert("Amount is Rs/-50");
+    console.log("Amount is Rs/-50");
+}
+
+if(name=="Sprite"){
+    alert("Amount is Rs/-50");
+    console.log("Amount is Rs/-50");
+}
+
+if(name=="Monster"){
+    alert("Amount is Rs/-40");
+    console.log("Amount is Rs/-40");
+}
+
+if(name=="Gatorade"){
+    alert("Amount is Rs/-25");
+    console.log("Amount is Rs/-25");
+}
+
+if(name=="Lemon Juice"){
+    alert("Amount is Rs/-20");
+    console.log("Amount is Rs/-20");
+}
+
+if(name=="Mountain Dew"){
+    alert("Amount is Rs/-50");
+    console.log("Amount is Rs/-50");
+}
+
+if(name=="Fanta"){
+    alert("Amount is Rs/-44");
+    console.log("Amount is Rs/-44");
+}
+
+if(name=="Dr Pepper"){
+    alert("Amount is Rs/-80");
+    console.log("Amount is Rs/-80");
+}
+
+if(name=="Tropicana"){
+    alert("Amount is Rs/-45");
+    console.log("Amount is Rs/-45");
+}
+
+if(name=="7-Up"){
+    alert("Amount is Rs/-47");
+    console.log("Amount is Rs/-47");
+}
+
+if(name=="Maaza"){
+    alert("Amount is Rs/-30");
+    console.log("Amount is Rs/-30");
+}
+
+if(name=="Spyci"){
+    alert("Amount is Rs/-50");
+    console.log("Amount is Rs/-50");
+}
+
+if(name=="Diet Coke"){
+    alert("Amount is Rs/-45");
+    console.log("Amount is Rs/-45");
+}
+if(name=="Coke Zero"){
+    alert("Amount is Rs/-40");
+    console.log("Amount is Rs/-40");
+}
+
+if(name=="Mirinda"){
+    alert("Amount is Rs/-70");
+    console.log("Amount is Rs/-70");
+}
+if(name=="Frooti"){
+    alert("Amount is Rs/-40");
+    console.log("Amount is Rs/-40");
+}
+
+
+}
